21709ea1e0baaf8ef26dbd3b806e1a1becc81100
WalterAltamirano <altamiranow4@gmail.com>
2026-07-10 13:27:46 -0300

    Endpoint funcionando. Avanzando en la logica y diseño

--- Web/src/components/Header.jsx
+++ Web/src/components/Header.jsx
@@ -1,7 +1,20 @@
+import '../styles/stylesHeader.css'
+import '../styles/index.css'
+
 const Header = () => {
     return(
         <header>
-            
+            <h1>Palabras Encadenadas</h1>
+            <nav>
+                <ul>
+                    <li>
+                        Ver mi historial
+                    </li>
+                    <li>
+                        TUki
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
--- Web/src/services/wordService.js
+++ Web/src/services/wordService.js
@@ -3,12 +3,8 @@ import axios from "axios";
 const baseUrl = "https://word-api-hmlg.vercel.app"
 
 export const getValidateWord = async (wordToVerify) => {
-    try {
-        const response = await axios.get(`${baseUrl}/api/validate?word=${wordToVerify}`);
-        console.log(response);
-        return response;
-    }catch(error) {
-        return error;
-    }
+        await axios.get(`${baseUrl}/api/validate?word=${wordToVerify}`,{})
+        .then((data) => console.log(data))
+        .catch((error) => error);
 
 }
